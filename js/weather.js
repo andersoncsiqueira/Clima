b745ec6db6b8af023486cdb9731e569dc6c174af
Anderson Siqueira <92267959+andersoncsiqueira@users.noreply.github.com>
2022-04-28 18:54:05 -0300

    cityDate

--- js/weather.js
+++ js/weather.js
@@ -1,1 +1,33 @@
 const apiKey = `0dc6M0X222AxkTeB8BcbN8A9DdiOWEnm`
+const city = `Juazeiro`
+
+let url = city => {
+    return `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${apiKey}&q=${city}`
+}
+
+let getDatas = async (url) => {
+
+    try {
+
+        const request = await fetch(url)
+    let [ cityData ] = await request.json()
+
+        if(!request.ok){
+
+            throw new Error(`Nao foi possivel obter os dados`)
+
+        }
+    
+        return cityData
+
+    } catch({name, message}){ 
+
+      alert(`${name}: ${message}`)
+        
+    }
+    
+}
+
+getDatas(url(`Juazeiro`))
+
+
