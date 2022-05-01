fdee24e646779a174824d371cb496f42497c11fb
Anderson Siqueira <92267959+andersoncsiqueira@users.noreply.github.com>
2022-05-01 17:36:43 -0300

    getcitydataweather

--- js/weather.js
+++ js/weather.js
@@ -1,23 +1,24 @@
 const apiKey = `0dc6M0X222AxkTeB8BcbN8A9DdiOWEnm`
-const city = `Juazeiro`
 
-let url = city => {
-    return `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${apiKey}&q=${city}`
+
+let url = cityName => {
+    return `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${apiKey}&q=${cityName}`
 }
 
-let getDatas = async (url) => {
+let getCityData = async cityName => {
 
     try {
-
-        const request = await fetch(url)
-    let [ cityData ] = await request.json()
+        const cityUrl = url(cityName)
+        const request = await fetch(cityUrl)
+    
 
         if(!request.ok){
 
             throw new Error(`Nao foi possivel obter os dados`)
 
         }
-    
+        
+        let [ cityData ] = await request.json()
         return cityData
 
     } catch({name, message}){ 
@@ -28,6 +29,35 @@ let getDatas = async (url) => {
     
 }
 
-getDatas(url(`Juazeiro`))
+
+let getCityWeather = async cityName => {
+
+    try {
+
+        const { Key } = await getCityData(cityName)
+        const cityWeatherUrl = `http://dataservice.accuweather.com/currentconditions/v1/${Key}?apikey=${apiKey}`
+        const request = await fetch(cityWeatherUrl)
+    
+
+        if(!request.ok){
+
+            throw new Error(`Nao foi possivel obter os dados`)
+
+        }
+        
+        let [ cityWeatherData ] = await request.json()
+        debugger
+        return cityWeatherData
+
+    } catch({name, message}){ 
+
+      alert(`${name}: ${message}`)
+        
+    }
+    
+}
+
+
+getCityWeather(`Juazeiro`)
 
 
